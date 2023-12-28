d885f0db65680cab830ff85674bf819d0fa4e3f5
divineanugrah <mdivineanugrah@gmail.com>
2023-12-28 21:40:48 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/musicapp/ApiInterface.kt
@@ -0,0 +1,16 @@
+package com.example.musicapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ApiInterface {
+
+    @Headers("X-RapidAPI-Key: c0cffcd0damshb59bdf61ae8405ap107044jsn6e097d03558a",
+        "X-RapidAPI-Host: deezerdevs-deezer.p.rapidapi.com")
+    @GET("search")
+    fun getData(@Query("q") query: String) : Call<MyData>
+
+
+}
